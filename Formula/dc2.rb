9b29d9aecdd68b3840929b03f4487ecada124456
Davide Mastromatteo <mastro35@gmail.com>
2025-07-23 15:52:28 +0200

    fixed link

--- Formula/dc2.rb
+++ Formula/dc2.rb
@@ -1,7 +1,7 @@
 class Dc2 < Formula
   desc "Dave's (RPN) Calculator - simple RPN calculator for terminal"
   homepage "https://github.com/mastro35/dc2"
-  url "https://github.com/mastro35/dc2/archive/refs/tags/dc2-0.1.0.tar.gz"
+  url "https://github.com/mastro35/dc2/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "ce8abc37ad013c1153288b451d56c7273128acd9751df95ad154bceb22a78428"
   license "GPL-2.0-or-later"
 
