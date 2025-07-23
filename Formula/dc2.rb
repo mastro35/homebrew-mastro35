779b11cefc5c6aecae968ab626c6303beabf0c30
Davide Mastromatteo <mastro35@gmail.com>
2025-07-23 15:51:23 +0200

    fixed sha

--- Formula/dc2.rb
+++ Formula/dc2.rb
@@ -1,8 +1,8 @@
 class Dc2 < Formula
   desc "Dave's (RPN) Calculator - simple RPN calculator for terminal"
   homepage "https://github.com/mastro35/dc2"
-  url "https://github.com/mastro35/dc2/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "<INSERISCI_SHA256_DEL_TAR_GZ>"
+  url "https://github.com/mastro35/dc2/archive/refs/tags/dc2-0.1.0.tar.gz"
+  sha256 "ce8abc37ad013c1153288b451d56c7273128acd9751df95ad154bceb22a78428"
   license "GPL-2.0-or-later"
 
   def install
