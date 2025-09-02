66132230bfdc1989fb758b7b3f3176241163bd8a
Davide Mastromatteo <mastro35@gmail.com>
2025-09-02 12:29:55 +0200

    Luka r0.5.0

--- Formula/luka.rb
+++ Formula/luka.rb
@@ -1,8 +1,8 @@
 class Luka < Formula
   desc "luka - a simple RPN calculator for terminal"
   homepage "https://github.com/mastro35/luka"
-  url "https://github.com/mastro35/luka/archive/refs/tags/v0.4.1.tar.gz"
-  sha256 "74349e95bc8784518102c6439ce73cf69ca8ec7fb1d69cfd8e008f41f625e29b"
+  url "https://github.com/mastro35/luka/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "c2689b19e9d051293426cb80d451c3fadbac58a40bec5606fc63945b45e37141"
   license "GPL-2.0"
 
   def install
